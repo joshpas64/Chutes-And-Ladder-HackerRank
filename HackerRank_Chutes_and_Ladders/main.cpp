7f6b383f986ffe7c88853a3f01f658a0d97baaa8
Joshua Pascascio <jpascasc@uci.edu>
2016-12-19 11:35:26 -0800

    Basic Comments for main

--- HackerRank_Chutes_and_Ladders/main.cpp
+++ HackerRank_Chutes_and_Ladders/main.cpp
@@ -5,8 +5,8 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
-    GameBoard gBoard = GameBoard();
-    gBoard.runGame();
+    cout << "Hello world!" << endl; //Default code with codeblocks can obviously omit
+    GameBoard gBoard = GameBoard(); //Create a gameboard
+    gBoard.runGame(); //Run the game and determine the minimum number of dice rolls for a certain combination of chutes and ladders
     return 0;
 }
