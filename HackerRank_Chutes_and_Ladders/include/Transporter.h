e6094f8433802777b1291f3b48f5a1686b9e45e1
Joshua Pascascio <jpascasc@uci.edu>
2016-12-18 20:37:27 -0800

    Comments on Chutes and Ladder Classes

--- HackerRank_Chutes_and_Ladders/include/Transporter.h
+++ HackerRank_Chutes_and_Ladders/include/Transporter.h
@@ -2,6 +2,12 @@
 #define TRANSPORTER_H
 #include "GameBoardException.h"
 
+/*
+Transporter objects are the actual chutes and ladders classes of the game. These are very simple objects with only methods being get and set 
+, looking back this could have been just a struct for the purpose of just this hackerRank problem. Obviously not every tile contains a
+chute or ladder so in this case instead we use Transporter pointers and denote the lack of a chute or ladder on a tile (meaning it is
+not a source or destination tile for a chute or ladder) with a nullptr
+*/
 class Transporter
 {
     public:
