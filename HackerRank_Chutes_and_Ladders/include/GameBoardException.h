22d5cb6b8506b83eb1f0920df730d4d8a73042c1
Joshua Pascascio <jpascasc@uci.edu>
2016-12-19 11:42:41 -0800

    Described the purpose of this exception class
    Also noted that by adding inline functions, only the header file would be needed, no .cpp source, since for this program , the GameBoardException class did not need much code or detail at all. It just needed to be a custom exception to be thrown, all the handling is done by the outside programs or code in the catch(GameBoardException &gE){} blocks

--- HackerRank_Chutes_and_Ladders/include/GameBoardException.h
+++ HackerRank_Chutes_and_Ladders/include/GameBoardException.h
@@ -1,7 +1,19 @@
 #ifndef GAMEBOARDEXCEPTION_H
 #define GAMEBOARDEXCEPTION_H
 
+/*
 
+GameBoardException was a basic exception to denote an instance of invalid user input when trying to initialize chutes or ladders, 
+most of its purpose was for it to be handled, caught, and differentitated from other exceptions in the main program. So as you can see there is 
+not much in terms of code, in reality you only need the header file to successfully build and use this class.
+
+Just in the constructor do something kind of like this
+
+public:
+    GameBoardException(){
+    };
+
+*/
 class GameBoardException
 {
     public:
